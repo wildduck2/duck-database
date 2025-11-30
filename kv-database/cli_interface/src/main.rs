aff8fefcf4ac6881305f5279ce0137a71f488627
wildduck2 <wezonaser50@gmail.com>
2025-11-30 20:46:00 +0200

    feat(kb-database): add segmentation and compaction to the process

--- kv-database/cli_interface/src/main.rs
+++ kv-database/cli_interface/src/main.rs
@@ -1,9 +1,26 @@
-use std::{fs, sync::Arc};
+use std::{
+  fs::{self, OpenOptions},
+  sync::Arc,
+};
 
 use core_engine::log_file;
 use ttlog::{
-  file_listener::FileListener, stdout_listener::StdoutListener, trace::Trace, ttlog_macros::debug,
+  file_listener::FileListener,
+  stdout_listener::StdoutListener,
+  trace::Trace,
+  ttlog_macros::{debug, trace},
 };
+
+// Pre-compaction steps
+// 1. Split the log file into multiple files (aka segments)
+// 2.
+
+// Compaction steps
+// 1. Read all the files
+// 2. Sort the files by timestamp
+// 3. Run compaction on each file
+// 4. Write the new file
+//
 
 fn main() -> Result<(), std::io::Error> {
   if fs::exists("./tmp")? {
@@ -16,18 +33,27 @@ fn main() -> Result<(), std::io::Error> {
   trace.set_level(ttlog::event::LogLevel::TRACE);
 
   let mut log_file = log_file::LogFile::new();
-  log_file.create("./tmp/log_file".to_string()).unwrap_or(());
+  log_file.create().unwrap_or(());
 
-  log_file.append("123", "{\"name\":\"wilddcuk\",\"age\":25}")?;
-  log_file.read("123")?;
-  log_file.delete("123")?;
+  for i in 0..100 {
+    log_file.append(&format!("123:{}", i), "{\"name\":\"wildduck\",\"age\":25}")?;
+  }
+  log_file.append("123:5", "{\"name\":\"wildduck\",\"age\":25}")?;
+  log_file.delete("123:1")?;
+
+  // log_file.append("123", "{\"name\":\"wilddcuk\",\"age\":25}")?;
+  // log_file.read("123")?;
+  // log_file.update("123", "{\"name\":\"wildduck\",\"age\":28}")?;
+  // log_file.read("123:1")?;
   // log_file.read("123")?;
 
-  let handle = std::thread::spawn(|| loop {
-    debug!("Waiting for compaction");
-    std::thread::sleep(std::time::Duration::from_secs(10));
+  // trace!("[LOGFILE]", file_size = log_file.get_file_size());
+
+  let handle = std::thread::spawn(move || loop {
+    let _ = log_file.compact();
+    std::thread::sleep(std::time::Duration::from_secs(100));
   });
 
-  handle.join();
+  let _ = handle.join();
   Ok(())
 }
--- kv-database/core_engine/src/log_file/mod.rs
+++ kv-database/core_engine/src/log_file/mod.rs
@@ -1,13 +1,15 @@
 use std::{
   collections::HashMap,
-  fs::{self, File, OpenOptions},
-  io::{self, Write},
-  os::unix::fs::FileExt,
+  fs::{self, metadata, File, OpenOptions},
+  io::{self, Read, Seek, Write},
+  os::unix::fs::{FileExt, MetadataExt},
 };
 
-use chrono::{TimeZone, Utc};
+use chrono::{offset, Utc};
 use serde;
 use ttlog::ttlog_macros::{error, info, trace};
+
+const FILE_THRESHOLD: u64 = 1024; // 1KB
 
 #[derive(Debug)]
 struct MetaIndex {
@@ -27,10 +29,11 @@ struct Index {
 
 #[derive(Debug)]
 pub struct LogFile {
-  index: HashMap<String, Index>,
   byte_offset: u64,
   current_file_id: u64,
   path: String,
+  data_index: HashMap<String, Index>,
+  pub file_index: HashMap<u64, String>,
 }
 
 impl Default for LogFile {
@@ -43,18 +46,28 @@ impl LogFile {
   pub fn new() -> Self {
     Self {
       path: "".to_string(),
-      byte_offset: 0,
+      byte_offset: 0x1,
       current_file_id: 0x1,
-      index: HashMap::new(),
+      data_index: HashMap::new(),
+      file_index: HashMap::new(),
     }
   }
 
-  pub fn create(&mut self, path: String) -> Result<(), std::io::Error> {
+  pub fn create(&mut self) -> Result<(), std::io::Error> {
     fs::create_dir_all("tmp")?;
+    let path = format!("./tmp/log-file-{}", self.current_file_id);
+
     OpenOptions::new().create(true).append(true).open(&path)?;
     self.path = path;
+    self
+      .file_index
+      .insert(self.current_file_id, self.path.clone());
+    self.byte_offset = 0;
 
-    trace!("Log file has been created successfully.");
+    trace!(
+      "[LOGFILE] Log file has been created successfully.",
+      file_id = self.current_file_id
+    );
     Ok(())
   }
 
@@ -64,6 +77,56 @@ impl LogFile {
       return Err(io::Error::other(""));
     }
 
+    let data_size = (value.len() + key.len() + 8 * 3) as u64;
+    let index_value = Index {
+      offset: self.byte_offset,
+      file_id: self.current_file_id,
+      total_size: data_size,
+    };
+
+    self.data_index.insert(key.to_string(), index_value);
+    self.byte_offset += data_size;
+
+    let timestamp = Utc::now().timestamp_nanos_opt().unwrap();
+
+    self.insert_index_value(MetaIndex {
+      timestamp,
+      key_size: key.len(),
+      key_buf: key.as_bytes().to_vec(),
+      value_size: value.len(),
+      value_buf: value.as_bytes().to_vec(),
+    })?;
+
+    info!("[WRITE]", index_value = value);
+
+    Ok(())
+  }
+
+  pub fn read(&mut self, id: &str) -> Result<String, io::Error> {
+    if !self.data_index.contains_key(id) {
+      return Err(io::Error::other("This key does not exist in the index"));
+    }
+
+    let index = self.get_index_value(id)?;
+
+    // let timestamp = Utc.timestamp_opt(index.timestamp, 0);
+    // let timestamp = timestamp.unwrap().to_string();
+    // let index_key_value = String::from_utf8(index.key_buf).unwrap().to_string();
+    let index_value_value = String::from_utf8(index.value_buf).unwrap().to_string();
+    info!("[READ]", index_value = index_value_value);
+    Ok(index_value_value)
+  }
+
+  pub fn update(&mut self, key: &str, value: &'static str) -> Result<(), io::Error> {
+    if key.is_empty() {
+      error!("The index length should be at least 1 character");
+      return Err(io::Error::other(""));
+    }
+
+    if !self.data_index.contains_key(key) {
+      return Err(io::Error::other("This key does not exist in the index"));
+    }
+
     let data_size = (value.len() + key.len() + 8 * 2) as u64;
     let index_value = Index {
       offset: self.byte_offset,
@@ -71,7 +134,16 @@ impl LogFile {
       total_size: data_size,
     };
 
-    self.index.insert(key.to_string(), index_value);
+    self.data_index.insert(key.to_string(), index_value);
+
+    let data_size = (value.len() + key.len() + 8 * 2) as u64;
+    let index_value = Index {
+      offset: self.byte_offset,
+      file_id: self.current_file_id,
+      total_size: data_size,
+    };
+
+    self.data_index.insert(key.to_string(), index_value);
     self.byte_offset += data_size;
 
     let timestamp = Utc::now().timestamp();
@@ -84,22 +156,9 @@ impl LogFile {
       value_buf: value.as_bytes().to_vec(),
     })?;
 
-    info!("[WRITE]", index_value = value);
+    info!("[UPDATE]", index_value = value);
+
     Ok(())
-  }
-
-  pub fn read(&mut self, id: &str) -> Result<String, io::Error> {
-    if !self.index.contains_key(id) {
-      return Err(io::Error::other("This key does not exist in the index"));
-    }
-
-    let index = self.get_index_value(id)?;
-    // let timestamp = Utc.timestamp_opt(index.timestamp, 0);
-    // let timestamp = timestamp.unwrap().to_string();
-    // let index_key_value = String::from_utf8(index.key_buf).unwrap().to_string();
-    let index_value_value = String::from_utf8(index.value_buf).unwrap().to_string();
-    info!("[READ]", index_value = index_value_value);
-    Ok(index_value_value)
   }
 
   pub fn delete(&mut self, id: &str) -> Result<String, io::Error> {
@@ -107,59 +166,126 @@ impl LogFile {
     let value = String::from_utf8(index.value_buf.clone())
       .unwrap()
       .to_string();
+    index.value_size = 0;
     index.value_buf.clear();
     self.insert_index_value(index)?;
-    self.index.remove(id);
+    self.data_index.remove(id);
 
     info!("[DELETE]", index_value = value);
     Ok("".to_string())
   }
 
-  pub fn compact(&self) {
-    println!("compacting data");
+  pub fn compact(&mut self) -> Result<(), io::Error> {
+    let new_hash = std::mem::take(&mut self.file_index);
+    let mut end_file = HashMap::<String, MetaIndex>::new();
+    let mut sorted_file_ids = new_hash.keys().collect::<Vec<_>>();
+    sorted_file_ids.sort();
+
+    for &file_id in sorted_file_ids {
+      let file_idx = new_hash.get(&file_id).unwrap();
+      self.compact_file(&mut end_file, file_idx)?
+    }
+    let _ = core::mem::replace(&mut self.file_index, new_hash);
+
+    let path = format!("./tmp/log-file-{}", self.current_file_id + 1);
+    let mut file = OpenOptions::new().create(true).append(true).open(&path)?;
+
+    for (_, value) in end_file.iter() {
+      file.write_all(&value.timestamp.to_le_bytes())?;
+      file.write_all(&value.key_size.to_le_bytes())?;
+      file.write_all(&value.key_buf)?;
+      file.write_all(&value.value_size.to_le_bytes())?;
+      file.write_all(&value.value_buf)?;
+    }
+
+    for (_, path) in self.file_index.iter() {
+      fs::remove_file(path)?;
+    }
+
+    self.current_file_id += 1;
+    self.file_index.insert(self.current_file_id, path);
+
+    info!("[COMPACT] Compaction has been completed successfully.");
+
+    Ok(())
   }
 
-  pub fn split(&self) {
-    println!("splitting data");
+  fn compact_file(
+    &mut self,
+    end_file: &mut HashMap<String, MetaIndex>,
+    file_idx: &String,
+  ) -> Result<(), io::Error> {
+    let mut offset = 0;
+    let file = File::open(file_idx)?;
+    let meta_data = fs::metadata(file_idx)?;
+
+    loop {
+      if meta_data.size() <= offset {
+        println!("Done with this file");
+        break;
+      }
+
+      let meta = self.get_index_from_file(&mut offset, &file)?;
+      let key = String::from_utf8(meta.key_buf.clone()).unwrap();
+
+      if meta.value_buf.is_empty() {
+        end_file.remove(&key);
+        continue;
+      }
+
+      end_file.insert(key, meta);
+    }
+
+    Ok(())
   }
 
   fn insert_index_value(&mut self, meta: MetaIndex) -> Result<(), io::Error> {
     let mut file = OpenOptions::new().append(true).open(&self.path)?;
+
     file.write_all(&meta.timestamp.to_le_bytes())?;
     file.write_all(&meta.key_size.to_le_bytes())?;
     file.write_all(&meta.key_buf)?;
     file.write_all(&meta.value_size.to_le_bytes())?;
     file.write_all(&meta.value_buf)?;
+    self.split()?;
 
     Ok(())
   }
 
   fn get_index_value(&mut self, id: &str) -> Result<MetaIndex, io::Error> {
-    let index = self.index.get(id).unwrap();
-    let file = File::open(&self.path)?;
-    let mut offset = index.offset;
+    if !self.data_index.contains_key(id) {
+      return Err(io::Error::other(""));
+    }
 
+    let index = self.data_index.get(id).unwrap();
+    let file = File::open(self.file_index.get(&index.file_id).unwrap())?;
+    let mut offset = index.offset;
+    self.get_index_from_file(&mut offset, &file)
+  }
+
+  fn get_index_from_file(&mut self, offset: &mut u64, file: &File) -> Result<MetaIndex, io::Error> {
     let mut ts_buff = [0u8; 8];
-    file.read_exact_at(&mut ts_buff, offset)?;
+    file.read_exact_at(&mut ts_buff, *offset)?;
     let timestamp = i64::from_le_bytes(ts_buff);
-    offset += 8;
+    *offset += 8;
 
     let mut key_size_buf = [0u8; 8];
-    file.read_exact_at(&mut key_size_buf, offset)?;
+    file.read_exact_at(&mut key_size_buf, *offset)?;
     let key_size = u64::from_le_bytes(key_size_buf) as usize;
-    offset += 8;
+    *offset += 8;
 
     let mut key_buf = vec![0u8; key_size];
-    file.read_exact_at(&mut key_buf, offset)?;
-    offset += key_size as u64;
+    file.read_exact_at(&mut key_buf, *offset)?;
+    *offset += key_size as u64;
 
     let mut value_size_buf = [0u8; 8];
-    file.read_exact_at(&mut value_size_buf, offset)?;
+    file.read_exact_at(&mut value_size_buf, *offset)?;
     let value_size = u64::from_le_bytes(value_size_buf) as usize;
-    offset += 8;
+    *offset += 8;
 
     let mut value_buf = vec![0u8; value_size];
-    file.read_exact_at(&mut value_buf, offset)?;
+    file.read_exact_at(&mut value_buf, *offset)?;
+    *offset += value_size as u64;
 
     Ok(MetaIndex {
       timestamp,
@@ -168,5 +294,22 @@ impl LogFile {
       value_size,
       value_buf,
     })
+  }
+
+  pub fn split(&mut self) -> Result<u64, io::Error> {
+    let metadata = fs::metadata(&self.path)?;
+
+    if metadata.size() > FILE_THRESHOLD {
+      trace!(
+        "[LOGFILE] File has exceeded the threshold",
+        threshold = FILE_THRESHOLD,
+        file_size = metadata.size()
+      );
+
+      self.current_file_id += 1;
+      self.create()?;
+    }
+
+    Ok(0)
   }
 }
